9bb1a4ce2367ce20a7d4a70d9d60ac78625f0c5a
RoguePi <rogelynpizon@gmail.com>
2026-01-10 02:27:17 +0800

    update solution from v15

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
-import ClientTermly from '../components/ClientTermly'
+import TermlyCMP from '../components/TermlyCMP'
+import { Suspense } from 'react'
 
 export const metadata = {
   title: 'Next.js v16 - Modern Web App',
@@ -14,8 +15,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ClientTermly />
         {children}
+        <Suspense fallback={null}>
+          <TermlyCMP 
+            websiteUUID="270c91dd-6788-48d0-823d-1e04be35bede"
+            autoBlock={true}
+          />
+        </Suspense>
       </body>
     </html>
   )
