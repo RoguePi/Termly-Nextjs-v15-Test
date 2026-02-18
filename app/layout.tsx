85a7f82036fc2ba081a9d61844ec7e4fac70c7af
RoguePi <rogelynpizon@gmail.com>
2026-02-18 08:06:07 +0800

    Added suppressHydration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Script from 'next/script'
 import TermlyCMP from '../components/TermlyCMP'
+import { Suspense } from "react";
 
 export const metadata = {
   title: 'Next.js v15 - Modern Web App',
@@ -14,9 +15,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <TermlyCMP websiteUUID="270c91dd-6788-48d0-823d-1e04be35bede" autoBlock />
+      <body suppressHydrationWarning={true}>
         {children}
+        <Suspense fallback={null}>
+          <TermlyCMP websiteUUID="270c91dd-6788-48d0-823d-1e04be35bede" autoBlock />
+        
+        {/* AudioEye - will be blocked by Termly */}        
         <Script 
           id="audioeye-script"
           strategy="afterInteractive"
@@ -24,6 +28,7 @@ export default function RootLayout({
             __html: `!function(){var b=function(){window.__AudioEyeSiteHash = "35bd0fbd79a49fdf33a0f3bc89f13a83"; var a=document.createElement("script");a.src="https://wsv3cdn.audioeye-services.com/aem.js?h=35bd0fbd79a49fdf33a0f3bc89f13a83";a.type="text/javascript";a.crossOrigin="anonymous";a.setAttribute("async","");document.getElementsByTagName("body")[0].appendChild(a)};"complete"!==document.readyState?window.addEventListener?window.addEventListener("load",b):window.attachEvent&&window.attachEvent("onload",b):b()}();`
           }}
         />
+         </Suspense>
       </body>
     </html>
   )
