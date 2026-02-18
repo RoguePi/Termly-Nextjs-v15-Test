cc6907ce52598763fa5beb687cc3f2f9382cf474
RoguePi <rogelynpizon@gmail.com>
2026-02-18 08:09:51 +0800

    remove comment

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,9 @@ import { Suspense } from "react";
 export const metadata = {
   title: 'Next.js v15 - Modern Web App',
   description: 'A beautiful and elegant Next.js application',
+  other: {
+    'permissions-policy': 'unload=()'
+  }
 }
 
 export default function RootLayout({
@@ -19,8 +22,7 @@ export default function RootLayout({
         {children}
         <Suspense fallback={null}>
           <TermlyCMP websiteUUID="270c91dd-6788-48d0-823d-1e04be35bede" autoBlock />
-        
-        {/* AudioEye - will be blocked by Termly */}        
+              
         <Script 
           id="audioeye-script"
           strategy="afterInteractive"
